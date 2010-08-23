94c00f8aa4b1f57b5844b1f70f28d6b9a3b7364d
kohsuke <kohsuke@dev.java.net>
2010-08-23 20:47:54 +0000

    compare with the baseline and report accordingly

--- src/main/java/hudson/plugins/maven_dump_test_dependencies/Dumper.java
+++ src/main/java/hudson/plugins/maven_dump_test_dependencies/Dumper.java
@@ -12,24 +12,30 @@ import org.apache.maven.project.MavenProject;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Kohsuke Kawaguchi
  */
 public class Dumper extends MavenReporter {
+    private transient Map<String,Long> last;
+
+
     @Override
     public boolean preExecute(MavenBuildProxy build, MavenProject pom, MojoInfo mojo, BuildListener listener) throws InterruptedException, IOException {
         if (isUnitTest(mojo))
-            dumpClasspath(pom,listener);
+            last = dumpClasspath(pom,listener,null);
         return super.preExecute(build, pom, mojo, listener);
     }
 
     @Override
     public boolean postExecute(MavenBuildProxy build, MavenProject pom, MojoInfo mojo, BuildListener listener, Throwable error) throws InterruptedException, IOException {
         if (isUnitTest(mojo))
-            dumpClasspath(pom,listener);
+            dumpClasspath(pom,listener,last);
         return super.postExecute(build, pom, mojo, listener, error);
     }
 
@@ -37,15 +43,23 @@ public class Dumper extends MavenReporter {
         return mojo.is("org.apache.maven.plugins", "maven-surefire-plugin", "test");
     }
 
-    private void dumpClasspath(MavenProject pom, BuildListener listener) {
+    private Map<String,Long> dumpClasspath(MavenProject pom, BuildListener listener, Map<String,Long> baseline) {
+        if (baseline==null) baseline = Collections.emptyMap();
+        Map<String,Long> data = new HashMap<String, Long>();
         try {
             for (String path : (List<String>)pom.getTestClasspathElements()) {
                 File f = new File(path);
-                listener.getLogger().println("[HUDSON] "+new Date(f.lastModified()).toLocaleString()+" : "+path);
+                long l = f.lastModified();
+                String timestamp = new Date(l).toLocaleString();
+                if (Long.valueOf(l).equals(baseline.get(path)))
+                    timestamp = "identical";
+                listener.getLogger().println("[HUDSON] "+ timestamp +" : "+path);
+                data.put(path,l);
             }
         } catch (DependencyResolutionRequiredException e) {
             e.printStackTrace(listener.error("Failed to dump classpath"));
         }
+        return data;
     }
 
     @Extension
